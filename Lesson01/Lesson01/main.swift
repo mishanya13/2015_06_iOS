f768d8a742b3f9d7a83accd7b63a501a400a0227
Elad Lavi <eladlavi1@gmail.com>
2015-06-17 21:20:46 +0300

    data types

---
+++ Lesson01/Lesson01/main.swift
@@ -0,0 +1,58 @@
+//
+//  main.swift
+//  Lesson01
+//
+//  Created by Elad Lavi on 6/17/15.
+//  Copyright (c) 2015 Elad Lavi. All rights reserved.
+//
+
+import Foundation
+
+var s = "Hello, World!";
+
+println(s);
+print("how ");
+print("are ");
+print("you?");
+
+let i:Int = -25;
+println("the value of i is \(i).");
+
+var firstName = "Elad";
+var lastName = "Lavi";
+println("my first name is \(firstName) and my last name is \(lastName)");
+//I can write here what ever I want. It won't get complied.
+
+/*
+this block won't get compiled either.
+
+*/
+
+let i1:UInt64 = 5;
+var i2 = 7;
+
+
+let f1:Float = 9.99;
+let f2 = 3.14;
+let d1:Double = 1.2345;
+
+let c1:Character = "A";
+let c2:Character = "A";
+println(c1 == c2);
+
+var b1:Bool;
+let b2 = false;
+b1 = true;
+println("the value of b1: \(b1)");
+
+
+
+
+
+
+
+
+
+
+
+
